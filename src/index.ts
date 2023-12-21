8b317b88c6175891d13976443149622d6195fa11
Made Indra <made.indra@protonmail.com>
2023-12-21 20:35:04 +0700

    added fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+import Fastify from 'fastify';
+
+const fastify = Fastify();
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+const HOST = process.env.HOST ?? '0.0.0.0';
+
+fastify.get('/', async (request, reply) => ({ hello: 'world' }));
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: PORT, host: HOST });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+// eslint-disable-next-line
+start();
