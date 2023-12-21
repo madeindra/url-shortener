f7b149bace0918ed51a6acef782838332c3e7610
Made Indra <made.indra@protonmail.com>
2023-12-21 20:15:08 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended-type-checked', // @typescript-eslint @v6
+    'plugin:@typescript-eslint/stylistic-type-checked', // @typescript-eslint @v6
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  }
+};
