98d25d0fe9ee30003051f41bd4062a746de6c9e5
Made Indra <made.indra@protonmail.com>
2023-12-22 20:33:25 +0700

    implemented redirect controller

--- src/controllers/redirect.ts
+++ src/controllers/redirect.ts
@@ -1,14 +1,28 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
+export interface RedirectControllerOptions {
+  tablename: string;
+}
+
 export default function redirectController(
   fastify: FastifyInstance,
-  opts: unknown,
+  options: RedirectControllerOptions,
   done: () => void,
 ) {
-  fastify.get('/:slug([a-zA-Z0-9]{5,})', (request: FastifyRequest, reply: FastifyReply) => {
+  fastify.get('/:slug([a-zA-Z0-9]{5,})', async (request: FastifyRequest, reply: FastifyReply) => {
     const { slug } = request.params as { slug: string };
 
-    return reply.redirect(301, `https://google.com/${slug}`);
+    const result: { original_url: string } | undefined = await fastify.sqlite.get(`SELECT original_url FROM ${options.tablename} WHERE slug = ?`, [slug]);
+
+    // if row doesn't exist, return 404
+    if (!result) {
+      return reply.code(404).send({ error: 'Not Found' });
+    }
+
+    // TODO: validate URL before redirecting
+
+    // if row exists, redirect to original_url
+    return reply.redirect(301, result.original_url);
   });
 
   done();
