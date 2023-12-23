d12db854e7fc078e0187e8755d0621d833007330
Made Indra <made.indra@protonmail.com>
2023-12-23 10:59:38 +0700

    added result copier

--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,33 @@
+// assign copying function to copy button
+const copyButton = document.getElementById('copyButton');
+
+// Initialize the tooltip without showing it initially
+const tooltip = new bootstrap.Tooltip(copyButton, {
+  placement: 'top',
+  title: 'Link copied!',
+  trigger: 'manual'
+});
+
+copyButton.onclick = () => {
+  // get the shortened URL input
+  const urlInput = document.getElementById('shortenedUrl');
+
+  urlInput.select();
+  urlInput.setSelectionRange(0, 99999);
+
+  // copy the text inside the text field
+  navigator.clipboard.writeText(urlInput.value);
+
+  // Show the tooltip after copying
+  tooltip.show();
+
+  // hide the tooltip after 1 seconds
+  setTimeout(() => {
+    tooltip.hide();
+  }, 1000);
+}
+
+// handle the form submission
 document.getElementById('urlForm').onsubmit = async (event) => {
   // prevent the form from submitting and refreshing the page
   event.preventDefault();
@@ -33,5 +63,8 @@ document.getElementById('urlForm').onsubmit = async (event) => {
   }
 
   // display the shortened URL
-  document.getElementById('shortenedUrl').innerText = data.shortUrl;
+  document.getElementById('shortenedUrl').value = data.shortUrl;
+
+  // remove invisible from resultDiv to display it
+  document.getElementById('resultDiv').classList.remove('invisible');
 };
